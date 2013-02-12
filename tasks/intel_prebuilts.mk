9bf885606cc9980c52a96b0cf62dc9183093ee3d
Olivier Monnier <olivier.monnier@intel.com>
2013-02-12 13:47:44 +0100

    Enhance automatic prebuilt generation framework
    BZ: 86409
    
    - Static module and copy headers classes are not installed on the system.
    So we need to use another variable independant of the installed modules.
    - this patch also creates a top level Android.mk that includes all makefiles
    under its tree
    
    Change-Id: Iae8692021b77f1337c6d0eea74e919131ba19ac0
    Signed-off-by: Olivier Monnier <olivier.monnier@intel.com>
    Reviewed-on: http://android.intel.com:8080/91496
    Reviewed-by: Alberdi, Ion <ion.alberdi@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Besanceney, VincentX <vincentx.besanceney@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- tasks/intel_prebuilts.mk
+++ tasks/intel_prebuilts.mk
@@ -1,8 +1,21 @@
+ifneq ($(TARGET_OUT_prebuilts),)
+intel_prebuilts_top_makefile := $(TARGET_OUT_prebuilts)/Android.mk
+$(intel_prebuilts_top_makefile):
+	@mkdir -p $(dir $@)
+	@echo 'LOCAL_PATH := $$(call my-dir)' > $@
+	@echo 'ifeq ($$(TARGET_ARCH),x86)' >> $@
+	@echo 'include $$(shell find $$(LOCAL_PATH) -mindepth 2 -name "Android.mk")' >> $@
+	@echo 'endif' >> $@
+endif
+
 .PHONY: intel_prebuilts
 $(foreach m, $(modules_to_install),\
 	$(eval intel_prebuilts: $(strip $(ALL_MODULES.$(m).PREBUILT_MAKEFILE))))
-intel_prebuilts:
-	@echo did make following prebuilts Android.mk:
-	@$(foreach m, $?,\
-		echo "    " $(m);)
-	@find $(TARGET_OUT_prebuilts) -name Android.mk -print -exec cat {} \;
+$(foreach m, $(INTEL_PREBUILTS_MAKEFILE),\
+	$(eval intel_prebuilts: $(strip $(m))))
+intel_prebuilts: $(intel_prebuilts_top_makefile)
+	@$(if $(TARGET_OUT_prebuilts), \
+		echo did make following prebuilts Android.mk: \
+		$(foreach m, $?,\
+			echo "    " $(m);) \
+		find $(TARGET_OUT_prebuilts) -name Android.mk -print -exec cat {} \;)
