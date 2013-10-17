313195333cbd9555c9fbbdbe54043abefa590d77
Jeremy Compostella <jeremy.compostella@intel.com>
2013-10-17 16:06:13 +0200

    ESP: ensure that efilinux is delivered as default and vendor OS loader
    BZ: 142848
    
    efilinux is now delivered as:
    - EFI/BOOT/boot$(ARCH).efi (where $(ARCH) is either ia32 either x86_64
      depending on the current build architecture option).
    - EFI/Intel/efilinux.efi
    
    Change-Id: I9392e71007b0a3da5ad75585ba7648c09ad15cf7
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: http://android.intel.com:8080/139640
    Reviewed-by: Chouleur, Sylvain <sylvain.chouleur@intel.com>
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Tested-by: Lumbierres, PhilippeX <philippex.lumbierres@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- tasks/UEFI.mk
+++ tasks/UEFI.mk
@@ -12,8 +12,18 @@ INSTALLED_ESPIMAGE_TARGET := $(BUILT_ESPIMAGE_TARGET)
 $(BUILD_ESPIMAGE_DIR):
 	$(hide) mkdir -p $(BUILD_ESPIMAGE_DIR)
 
+ifeq ($(TARGET_ARCH),x86)
+	EFI_ARCH := ia32
+endif
+ifeq ($(TARGET_ARCH),x86-64)
+	EFI_ARCH := x86_64
+endif
+
 $(INSTALLED_ESPIMAGE_TARGET): $(BUILD_ESPIMAGE_DIR) efilinux | $(HOST_OUT_EXECUTABLES)/mcopy
 	$(call pretty,"Target ESP image: $@")
+	$(hide) mkdir -p $(PRODUCT_OUT)/esp/EFI/BOOT $(PRODUCT_OUT)/esp/EFI/Intel
+	$(hide) ln -sf ../../../efilinux.efi $(PRODUCT_OUT)/esp/EFI/BOOT/boot$(EFI_ARCH).efi
+	$(hide) ln -sf ../../../efilinux.efi $(PRODUCT_OUT)/esp/EFI/Intel/
 	$(hide) vendor/intel/support/make_vfatfs "ESP" \
 		$(BUILT_ESPIMAGE_TARGET) \
 		$(shell python -c 'import json; print json.load(open("'$(PART_MOUNT_OVERRIDE_FILE)'"))["partitions"]["ESP"]["size"]') \
