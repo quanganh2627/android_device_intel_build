2c7224898e474ed556d5e4070b169da80dd7628f
Benoit Fradin <benoit.fradin@intel.com>
2014-03-17 10:20:22 +0100

    [UEFI] Remove copy of capsule.bin in ESP.img in UEFI.mk
    BZ: 179495
    
    Removed copy of capsule.bin in ESP.
    
    Change-Id: I9dc131ce83de39d9a1659492d53cc13bce6ba7d6
    Signed-off-by: Benoit Fradin <benoit.fradin@intel.com>

--- tasks/UEFI.mk
+++ tasks/UEFI.mk
@@ -6,9 +6,6 @@ INTERNAL_ESPIMAGE_FILES := $(filter $(TARGET_ROOT_OUT)/esp/%, \
 
 BUILD_ESPIMAGE_DIR := $(PRODUCT_OUT)/esp
 
-BIOS_CAPSULE_INPUT_FILE := $(FW_INGREDIENTS_PATHS)/capsule.bin
-BIOS_CAPSULE_OUTPUT_FILE := BiosUpdate.fv
-
 $(BUILD_ESPIMAGE_DIR):
 	$(hide) mkdir -p $(BUILD_ESPIMAGE_DIR)
 
@@ -28,13 +25,6 @@ $(INSTALLED_ESPIMAGE_TARGET): $(BUILD_ESPIMAGE_DIR) $(EFILINUX_MODULE) warmdump 
 	$(hide) $(ACP) $(PRODUCT_OUT)/$(EFILINUX_MODULE).efi $(PRODUCT_OUT)/esp/EFI/Intel/efilinux.efi
 	$(hide) $(ACP) $(PRODUCT_OUT)/warmdump.efi $(PRODUCT_OUT)/esp/EFI/Intel/
 
-ifeq ($(BOARD_HAS_CAPSULE),true)
-ifneq (,$(wildcard $(BIOS_CAPSULE_INPUT_FILE)))
-	$(hide) $(ACP) $(BIOS_CAPSULE_INPUT_FILE) $(PRODUCT_OUT)/esp/$(BIOS_CAPSULE_OUTPUT_FILE)
-else
-	$(hide) echo "$(BIOS_CAPSULE_INPUT_FILE) file doesn't exist, not included in $(INSTALLED_ESPIMAGE_TARGET)"
-endif
-endif
 	$(hide) vendor/intel/support/make_vfatfs "ESP" \
 		$(INSTALLED_ESPIMAGE_TARGET) \
 		$(shell python -c 'import json; print json.load(open("'$(PART_MOUNT_OVERRIDE_FILE)'"))["partitions"]["ESP"]["size"]') \
