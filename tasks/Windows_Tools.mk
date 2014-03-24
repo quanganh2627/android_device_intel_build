122c23ca6d6a02734402f5391c3cc679b8c1bf6d
Jocelyn Falempe <jocelyn.falempe@intel.com>
2014-03-24 15:33:44 +0100

    move publish recipes to legacy_publish.mk
    BZ: 181390
    
    Change-Id: I3cc735669526204097449bdb1b6d11cd78f0fc1c
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>

--- tasks/Windows_Tools.mk
+++ tasks/Windows_Tools.mk
@@ -1,15 +1,12 @@
 # Define publish target for windows tools
-PUBLISH_PATH:= $(TOP)/pub
-PUBLISH_TOOLS_PATH := $(abspath $(PUBLISH_PATH)/tools/)
-PUBLISH_WINDOWS_TOOLS_deps := \
-	$(HOST_OUT_EXECUTABLES)/adb.exe \
-	$(HOST_OUT_EXECUTABLES)/AdbWinUsbApi.dll \
-	$(HOST_OUT_EXECUTABLES)/AdbWinApi.dll \
-	$(HOST_OUT_EXECUTABLES)/fastboot.exe
-publish_windows_tools: $(PUBLISH_WINDOWS_TOOLS_deps)
-	@ echo "Publish windows tools"
-	@ mkdir -p $(PUBLISH_TOOLS_PATH)
-	(cd out/host/ && cp --parents windows-x86/bin/adb.exe $(PUBLISH_TOOLS_PATH))
-	(cd out/host/ && cp --parents windows-x86/bin/AdbWinUsbApi.dll $(PUBLISH_TOOLS_PATH))
-	(cd out/host/ && cp --parents windows-x86/bin/AdbWinApi.dll $(PUBLISH_TOOLS_PATH))
-	(cd out/host/ && cp --parents windows-x86/bin/fastboot.exe $(PUBLISH_TOOLS_PATH))
+
+winpub_tools := adb.exe AdbWinUsbApi.dll AdbWinApi.dll fastboot.exe
+PUBLISH_WINDOWS_TOOLS_deps := $(addprefix $(HOST_OUT_EXECUTABLES)/,$(winpub_tools))
+
+publish_windows_tools: $(PUBLISH_WINDOWS_TOOLS_deps) | $(ACP)
+	@echo "Publish windows tools"
+	$(hide) mkdir -p $(PUBLISH_TOOLS_PATH)/windows-x86/bin
+	$(hide) $(ACP) $^ $(PUBLISH_TOOLS_PATH)/windows-x86/bin/
+
+
+
---
+++ tasks/zflashfiles.mk
@@ -0,0 +1,6 @@
+PUBLISH_PATH := pub
+PUBLISH_TOOLS_PATH := $(PUBLISH_PATH)/tools
+
+ifneq ($(LEGACY_PUBLISH),true)
+# Not implemented Yet !
+endif # LEGACY_PUBLSH != true
