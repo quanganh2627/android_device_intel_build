4b8b6be78ecbbf51142dc74eb74429992533a066
Jocelyn Falempe <jocelyn.falempe@intel.com>
2014-04-25 10:38:15 +0200

    Add generic rules for uefi executable.
    BZ: 190012
    
    Change-Id: Ifb40911a5d4a0de3af7d3fe0366e9ef219af0efd
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>

--- core/definitions.mk
+++ core/definitions.mk
@@ -19,3 +19,22 @@ include $(BUILD_PREBUILT)
 
 endef
 
+# Additional rules for uefi apps
+ifeq ($(TARGET_ARCH),x86)
+    EFI_ARCH := ia32
+    EFI_TARGET_LIBGCC := $(dir $(TARGET_LIBGCC))
+endif
+
+ifneq (,$(filter $(TARGET_ARCH),x86-64)$(filter $(BOARD_USE_64BIT_KERNEL),true))
+    EFI_ARCH := x86_64
+    EFI_TARGET_LIBGCC := $(patsubst %/32/,%,$(dir $(TARGET_LIBGCC)))
+endif
+
+EFI_ARCH_CFLAGS_ia32 := -m32 -DCONFIG_X86
+EFI_ARCH_CFLAGS_x86_64 := -m64 -DCONFIG_X86_64
+EFI_EXTRA_CFLAGS := $(EFI_ARCH_CFLAGS_$(EFI_ARCH)) -fPIC -fPIE \
+    -fshort-wchar -ffreestanding -Wall -fstack-protector \
+    -Wl,-z,noexecstack -O2 -D_FORTIFY_SOURCE=2 \
+    -g -fno-merge-constants
+
+BUILD_UEFI_EXECUTABLE := bootable/uefi/uefi_executable.mk
