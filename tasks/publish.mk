9aa303592dcd1ea84f317bad17d1d4b5c6fc7c61
Huzefa Kankroliwala <huzefa.nomanx.kankroliwala@intel.com>
2014-10-14 15:46:58 -0700

    build: get intel_prebuilts target enabled
    The intel_prebuilts.mk is missing in the current build
    setup. This is required to generate prebuilts for external
    release. This file is ported from imin_legacy
    
    Also ported a change from imin_legacy to publish prebuilts.zip
    if EXTERNAL_BINARIES=true for intel_prebuilts target
    
    Change-Id: Ib4e38b397f66f158c81ff238916f0ebc8836903d
    Signed-off-by: Huzefa Kankroliwala <huzefa.nomanx.kankroliwala@intel.com>
    Reviewed-on: https://android.intel.com/288289
    Reviewed-by: sys_pccgand <sys_pccgand@intel.com>

--- tasks/publish.mk
+++ tasks/publish.mk
@@ -45,6 +45,28 @@ publish_liveimage:
 	@echo "Warning: Unable to fulfill publish_liveimage makefile request"
 endif
 
+PUB_INTEL_PREBUILTS := $(TOP)/pub/$(TARGET_PUBLISH_PATH)/prebuilts.zip
+
+EXTERNAL_CUSTOMER ?= "g"
+
+INTEL_PREBUILTS_LIST := $(shell repo forall -g bsp-priv -a $(EXTERNAL_CUSTOMER)_external=bin -p -c echo 2> /dev/null)
+INTEL_PREBUILTS_LIST := $(filter-out project,$(INTEL_PREBUILTS_LIST))
+INTEL_PREBUILTS_LIST := $(addprefix prebuilts/intel/, $(subst /PRIVATE/,/prebuilts/$(REF_PRODUCT_NAME)/,$(INTEL_PREBUILTS_LIST)))
+INTEL_PREBUILTS_LIST += prebuilts/intel/Android.mk
+
+$(PUB_INTEL_PREBUILTS): generate_intel_prebuilts
+	@echo "Publish prebuilts for external release"
+	$(hide) rm -f $@
+	$(hide) cd $(PRODUCT_OUT) && zip -r $(abspath $@) $(INTEL_PREBUILTS_LIST)
+
+# publish external if buildbot set EXTERNAL_BINARIES env variable
+# and only for userdebug
+ifeq (userdebug,$(TARGET_BUILD_VARIANT))
+ifeq ($(EXTERNAL_BINARIES),true)
+publish_intel_prebuilts: publish_mkdir_dest $(PUB_INTEL_PREBUILTS)
+endif
+endif
+
 .PHONY: publish_factoryscripts
 ifdef FACTORY_SCRIPTS_PACKAGE_TARGET
 publish_factoryscripts: publish_mkdir_dest $(FACTORY_SCRIPTS_PACKAGE_TARGET)
