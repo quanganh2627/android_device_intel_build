121d5bdfdf08499fddfd0e8a69fc69bc9ee38490
Huzefa Kankroliwala <huzefa.nomanx.kankroliwala@intel.com>
2014-10-16 17:50:55 -0700

    Revert "build: generate *-factory.tgz binary in pub folder"
    This reverts commit e7517fe225bbf94a4bf67c6277663c0fda12ffbb
    This is done now in publish.mk
    
    Change-Id: Id33f5da01ff8ddf94c5d0eb96763c2840d958063
    Signed-off-by: Huzefa Kankroliwala <huzefa.nomanx.kankroliwala@intel.com>
    Reviewed-on: https://android.intel.com/290261
    Reviewed-by: sys_pccgand <sys_pccgand@intel.com>
    Reviewed-by: Hicks, Leesa <leesa.hicks@intel.com>
    Tested-by: Hicks, Leesa <leesa.hicks@intel.com>

--- tasks/factory_scripts.mk
+++ tasks/factory_scripts.mk
@@ -4,7 +4,6 @@
 # FIXME why the conversion to lowercase??
 BUILD_ID_LC := $(shell echo $(BUILD_ID) | tr A-Z a-z)
 FACTORY_SCRIPTS_PACKAGE_TARGET := $(PRODUCT_OUT)/$(TARGET_PRODUCT)-$(BUILD_ID_LC)-factory.tgz
-FACTORY_SCRIPTS_PACKAGE_PUB := $(PUB_PATH)/$(TARGET_PRODUCT)-$(BUILD_ID_LC)-factory.tgz
 FACTORY_SCRIPTS_RADIO := $(if $(BOARD_RADIOIMAGE), --radio $(BOARD_RADIOIMAGE))
 
 $(FACTORY_SCRIPTS_PACKAGE_TARGET): \
@@ -24,26 +23,8 @@ $(FACTORY_SCRIPTS_PACKAGE_TARGET): \
 		--sleeptime 45 \
 		--output $@
 
-$(FACTORY_SCRIPTS_PACKAGE_PUB): \
-		device/intel/build/generate_factory_images \
-		$(PRODUCT_OUT)/bootloader \
-		$(INTERNAL_UPDATE_PACKAGE_TARGET) \
-		$(BOARD_RADIOIMAGE) \
-		$(DISTTOOLS) $(SELINUX_DEPENDS) \
-
-	@echo "Package: $@"
-	# Generate Package
-	$(hide) ./device/intel/build/generate_factory_images \
-		--product $(TARGET_PRODUCT) --release $(BUILD_ID) \
-		--bootloader $(PRODUCT_OUT)/bootloader \
-		--update-archive $(INTERNAL_UPDATE_PACKAGE_TARGET) \
-		$(FACTORY_SCRIPTS_RADIO) \
-		--sleeptime 45 \
-		--output $@
-
 .PHONY: factoryscripts
 factoryscripts: $(FACTORY_SCRIPTS_PACKAGE_TARGET)
-factoryscripts: $(FACTORY_SCRIPTS_PACKAGE_PUB)
 
 ifeq ($(TARGET_BUILD_INTEL_FACTORY_SCRIPTS),true)
 $(call dist-for-goals,droidcore,$(FACTORY_SCRIPTS_PACKAGE_TARGET))
